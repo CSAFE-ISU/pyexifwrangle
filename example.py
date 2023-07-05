10b3a3ac75d17bfce9aac4e4a1fff01bba80b76f
Stephanie Reinders <reinders.stephanie@gmail.com>
2023-07-05 11:38:31 -0500

    Created a wrapper function run_checks_for_model() to run a bunch of checks at once on images from a single model.
    The function allows the user to choose which columns to check and whether to check them with images grouped by camera type or all images together.

--- example.py
+++ example.py
@@ -5,14 +5,7 @@ path = 'tests/fixtures/exif.csv'
 file_col_name = 'SourceFile'
 df = wrangle.read_exif(path, file_col_name=file_col_name)
 
-# Are any images missing EXIF data?
-missing = wrangle.check_missing_exif(df=df, file_col_name=file_col_name)
-
-# Do all cameras have 100 images?
-total = wrangle.count_images_by_camera(df=df, file_col_name=file_col_name)
-
-# Is the aperture the same for each type of camera?
-front = wrangle.check_column(df=df, file_col_name=file_col_name, col_name='Aperture', by_camera='Front')
-telephoto = wrangle.check_column(df=df, file_col_name=file_col_name, col_name='Aperture', by_camera='Telephoto')
-wide = wrangle.check_column(df=df, file_col_name=file_col_name, col_name='Aperture', by_camera='Wide')
-ultra = wrangle.check_column(df=df, file_col_name=file_col_name, col_name='Aperture', by_camera='Ultra')
+all_fields = ['DigitalZoomRatio', 'ExposureMode']
+camera_fields = ['Aperture', 'ImageSize']
+wrangle.run_checks_for_model(df=df, file_col_name=file_col_name, model_name='Note10',
+                             all_fields=all_fields, camera_fields=camera_fields, output_dir='data')
