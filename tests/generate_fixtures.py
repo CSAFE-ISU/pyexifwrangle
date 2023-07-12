a6edeedacf2502e2e44e4e235f698d9918c8a8b3
Stephanie Reinders <reinders.stephanie@gmail.com>
2023-07-12 13:26:27 -0500

    Simplified functions.
    The main functions are now
    
    1. read_exif() to load the EXIF data from a csv file and drop and images that have names that start with '.'
    2. filenames2columns() which allows the user to split the folders in the file paths in the filename column into individual columns and choose names for the new columns.
    3. check_missing_exif() allows the user to specify a column in the EXIF data frame and returns any images with a null value in that column.
    4. count_images_by_columns() allows the user group the EXIF data frame by a list of column(s) and count the number of images per group. Optionally, the user can also enter a list of column(s) to sort the grouped data frame.
    
    Now there is a single helper function:
    
    1. get_column() extracts the specified column from the EXIF data frame and returns it as a list.
    
    All tests pass.

--- tests/generate_fixtures.py
+++ tests/generate_fixtures.py
@@ -1,15 +1,30 @@
 import pyexifwrangle.wrangle as wrangle
 
 
-path = 'tests/fixtures/exif.csv'
-file_col_name = 'SourceFile'
-df = wrangle.read_exif(path=path, file_col_name=file_col_name)
+path = 'tests/fixtures/exif_s21.csv'
+filename_col = 'SourceFile'
 
-total = wrangle.count_images_by_camera(df=df, file_col_name=file_col_name)
-total.to_csv("tests/fixtures/totals.csv", index=False)
+# make csv smaller - sample 10 images per camera and select s21_1 and s21_2 only
+df = wrangle.read_exif(path, filename_col=filename_col)
+df = wrangle.filename2columns(df=df, filename_col=filename_col,
+                              columns=['model', 'phone', 'scene_type', 'camera', 'image'])
+df = df.groupby(['model', 'phone', 'scene_type', 'camera']).sample(n=10).reset_index(drop=True)
+keep = ['s21_1', 's21_2']
+df = df.query('phone in @keep').reset_index(drop=True)
+df.to_csv('tests/fixtures/exif_s21.csv', index=False)
 
-ap = wrangle.check_columns(df=df, file_col_name=file_col_name, col_names=['Aperture'])
-ap.to_csv("tests/fixtures/apertures.csv", index=False)
+# load smaller csv
+df = wrangle.read_exif(path, filename_col=filename_col)
+df = wrangle.filename2columns(df=df, filename_col=filename_col,
+                              columns=['model', 'phone', 'scene_type', 'camera', 'image'])
+df.to_csv('tests/fixtures/exif_s21_columns.csv', index=False)
 
-ap = wrangle.check_columns(df=df, file_col_name=file_col_name, col_names=['Aperture'], by_camera=['Front'])
-ap.to_csv("tests/fixtures/front_apertures.csv", index=False)
+missing = wrangle.check_missing_exif(df=df, column='Aperture').reset_index(drop=True)
+missing.to_csv('tests/fixtures/missing.csv', index=False)
+
+counts = wrangle.count_images_by_columns(df=df, columns=['model', 'phone', 'scene_type', 'camera'])
+counts.to_csv('tests/fixtures/counts.csv', index=False)
+
+aperture = wrangle.count_images_by_columns(df=df, columns=['model', 'phone', 'scene_type', 'camera', 'Aperture'],
+                                           sort=['model', 'camera', 'phone', 'scene_type'])
+aperture.to_csv('tests/fixtures/aperture_sorted.csv', index=False)
